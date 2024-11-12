d6da6b16ad875819b8412780fed4be05fc77f16e
Lucasvpl <lucasvpldeveloper@gmail.com>
2024-11-11 23:50:58 -0300

    Entendendo JSX

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,7 +1,13 @@
 export default function Titulo() {
+
+    let nome = "Lucas Vinicius"
+    const soma = 10 + 10; 
+    const urlImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+
     return (
         <>
-        <h1>Oi eu sou um título</h1>
+        <h1>Oi eu sou um {soma}</h1>
+        <img width={100} src={urlImg} />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ex molestiae similique autem quidem numquam nesciunt magni. Explicabo nam accusantium perspiciatis rerum voluptas, sed voluptatum unde eaque nobis temporibus necessitatibus!</p>
         </>
     )
