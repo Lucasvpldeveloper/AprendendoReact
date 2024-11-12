7d0809ea463f819655bda491467e7e7a469453e4
Lucasvpl <lucasvpldeveloper@gmail.com>
2024-11-11 23:45:45 -0300

    componentes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from "./Titulo"
+
+export default function App() {
+  return (
+    <>
+    <h1>Olá mundo!</h1>
+    <Titulo/>
+    <Titulo/>
+    <Titulo/>
+    <Titulo/>
+    </>
+   
+  )
+}
---
+++ src/Titulo.jsx
@@ -0,0 +1,8 @@
+export default function Titulo() {
+    return (
+        <>
+        <h1>Oi eu sou um título</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ex molestiae similique autem quidem numquam nesciunt magni. Explicabo nam accusantium perspiciatis rerum voluptas, sed voluptatum unde eaque nobis temporibus necessitatibus!</p>
+        </>
+    )
+}
